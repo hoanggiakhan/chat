ba882a9f6a462274c098e85b0d3e5859a9cc8b33
hoanggiakhan <147174692+hoanggiakhan@users.noreply.github.com>
2025-01-12 14:28:09 +0700

    Update main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,7 +23,7 @@ function connect(event) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('/https://chat-ku62.onrender.com');
+        var socket = new SockJS('https://chat-ku62.onrender.com/javatechie');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
